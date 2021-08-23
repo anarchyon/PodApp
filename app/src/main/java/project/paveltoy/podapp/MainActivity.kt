ce484a85e0196376ffaf7a9655139b72449cd704
toygildinpv <protagonyst@mail.ru>
2021-08-24 00:26:22 +0400

    project created

---
+++ app/src/main/java/project/paveltoy/podapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package project.paveltoy.podapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
