25191fbdfc4841e37768d4365e547931f916a252
anarchyon <protagonyst@mail.ru>
2021-08-29 08:51:18 +0300

    Исправление по замечаниям к домашке lesson1.  Исправление нейминга в дата-классе Apod.kt с использованием аннотации @SerializedName()

--- app/src/main/java/project/paveltoy/podapp/ui/apod/ApodFragment.kt
+++ app/src/main/java/project/paveltoy/podapp/ui/apod/ApodFragment.kt
@@ -1,9 +1,7 @@
 package project.paveltoy.podapp.ui.apod
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
